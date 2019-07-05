99a1f2e0e56135bf4601d931d5f8eb1bcf851b68
Emersonn <emersondinizz@yahoo.com.br>
2019-07-05 08:23:09 -0400

    ajuste tela falar e frase

--- Esperanza/Esperanza/Esperanza.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Esperanza/Esperanza/Esperanza.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "0cb3f8eb-4a0d-4955-8069-71baee4ace4a",
+		"XAMARIN_BUILD_ID", "6eed2ab4-6c5b-4bbb-92a2-fe6c0350e943",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep",
